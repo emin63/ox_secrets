3413ded5997340866181d4204bf2fd0d46ae2067
Emin Martinian <emin.martinian@gmail.com>
2022-03-09 13:55:56 -0500

    Improved settings to read from env and also replace category

--- ox_secrets/settings.py
+++ ox_secrets/settings.py
@@ -6,14 +6,24 @@ import os
 # Specifies which type of secret server you
 # want to use. Can change this at run-time
 # or override with environment variable OX_SECRETS_MODE
-OX_SECRETS_MODE = 'FILE'
+OX_SECRETS_MODE = os.environ.get('OX_SECRETS_MODE', 'FILE')
 
 # Specifies default location of secrets file for server from
 # file system in fss.py. Default is ~/.ox_secrets. You can
 # override either with environment variable OX_SECRETS_FILE
 # or by changing below at run-time.
-OX_SECRETS_FILE = os.path.join(os.environ.get('HOME', '/'), '.ox_secrets.csv')
+OX_SECRETS_FILE = os.path.join(os.environ.get(
+    'OX_SECRETS_FILE', os.environ.get('HOME', '/')), '.ox_secrets.csv')
 
 
 # Used for the aws secrets manager. Specifies the profile name in your ~/.aws.
 OX_SECRETS_AWS_PROFILE_NAME = None
+
+# If you use '^prod/' for OX_SECRETS_CATEGORY_REGEXP and
+# use 'test/' for OX_SECRETS_CATEGORY_REPLACE then you will
+# automatically have the all categories starting with 'prod/' replaced
+# with 'test/' in calls to get_secret.
+OX_SECRETS_CATEGORY_REGEXP = os.environ.get(
+    'OX_SECRETS_CATEGORY_RE', None)
+OX_SECRETS_CATEGORY_REPLACE = os.environ.get(
+    'OX_SECRETS_CATEGORY_REPLACE', None)
